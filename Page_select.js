8637bad2279c560cbbccacaa6e455c0014e88c9d
kaiseng <tankaiseng98@hotmail.com>
2018-10-01 17:51:14 +0800

    fourth commit

--- Page_select.js
+++ Page_select.js
@@ -1,3 +1,23 @@
+var ListOfBookmark= document.getElementById("ListOfBookmark");
+var TitleOfPage
+
+function pageurl(TitleOfPage) {
+    switch (TitleOfPage) {
+        case "PSB Academy Portal":
+            return browser.extension.getURL("https://www.psb-academy.edu.sg");
+        case "PSB Moodle":
+            return browser.extension.getURL("https://lms.psb-academy.edu.sg/");
+        case "Coventry University Portal":
+            return browser.extension.getURL("https://www.coventry.ac.uk");
+        case "Coventry University Solar":
+            return browser.extension.getURL("https://webapp.services.coventry.ac.uk/Apps/Student/0/Login.htm");
+        case "Google Scholar":
+            return browser.extension.getURL("https://scholar.google.com.sg/");
+        case "Wikipedia":
+            return browser.extension.getURL("https://www.wikipedia.org/");
+    }
+}
+
 function isSupportedProtocol(urlString) {
     var supportedProtocols = ["https:", "http:", "ftp:", "file:"];
     var url = document.createElement('a');
